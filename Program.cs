52ca17abbb68e5caacb5d68542cc7cb39f23c87c
Ergashev21 <t.ergashev2120@gmail.com>
2026-04-30 11:56:31 +0500

    Tayyor1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Ikki son ustida amallar ");
+Console.Write("a sonini kiriting : ");
+string inputFirst = Console.ReadLine();
+int a = Convert.ToInt32(inputFirst);
+Console.Write("b sonini kiriting : ");
+string inputSecond = Console.ReadLine();
+int b = Convert.ToInt32(inputSecond);
+Console.WriteLine("Yig'indi : " + (a + b));
+Console.WriteLine("Ayirma : " + (a - b));
+Console.WriteLine("Ko'paytma : " + (a * b));
+Console.WriteLine("Bo'linma : " + (a / b));
+
+/*string userInput = Console.ReadLine();
+decimal userNumber = Convert.ToDecimal(userInput);
+Console.WriteLine(userNumber+1);*/
