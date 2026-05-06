5e19e98192c83c3b6a10d80516c97df8a471aa5d
Ergashev21 <t.ergashev2120@gmail.com>
2026-05-06 09:37:53 +0500

    Completed

--- Program.cs
+++ Program.cs
@@ -71,83 +71,96 @@ double y2 = Convert.ToDouble(Console.ReadLine());
 
 }*/
 // 5-dars vazifasi 
-Console.WriteLine("Ushbu dastur orqali \n \t 1: Arifmetik amallarni xisoblash \n\t 2: Geron Formulasi  ");
- string str = Console.ReadLine();
- int userInput = Convert.ToInt32(str);
- switch (userInput)
+bool s = true;
+while(s)
 {
-    case 1:
-        {
-            Console.Write("Ikki son ustida amallar ");
-            Console.Write("a sonini kiriting : ");
-            string inputFirst = Console.ReadLine();
-            int a = Convert.ToInt32(inputFirst);
-            Console.Write("b sonini kiriting : ");
-            string inputSecond = Console.ReadLine();
-            int b = Convert.ToInt32(inputSecond);
-            Console.Write("Amalni kiriting (+, -, *, /): ");
-            string amal = Console.ReadLine();
-            if (amal == "+")
+    Console.Clear();
+    Console.WriteLine("Ushbu dastur orqali \n \t 1: Arifmetik amallarni xisoblash \n\t 2: Geron Formulasi \n\t 3:Sonning kvadratini qaytarish ");
+    string str = Console.ReadLine();
+    int userInput = Convert.ToInt32(str);
+    switch (userInput)
+    {
+        case 1:
             {
-                Console.WriteLine("Natija: " + (a + b));
-            }
-            else if (amal == "-")
-            {
-                Console.WriteLine("Natija: " + (a - b));
-            }
-            else if (amal == "*")
-            {
-                Console.WriteLine("Natija: " + (a * b));
-            }
-            else if (amal == "/")
-            {
-                if (b == 0)
-                    Console.WriteLine("0 ga bolish mumkin emas ");
+                Console.Write("Ikki son ustida amallar ");
+                Console.Write("a sonini kiriting : ");
+                string inputFirst = Console.ReadLine();
+                int a = Convert.ToInt32(inputFirst);
+                Console.Write("b sonini kiriting : ");
+                string inputSecond = Console.ReadLine();
+                int b = Convert.ToInt32(inputSecond);
+                Console.Write("Amalni kiriting (+, -, *, /): ");
+                string amal = Console.ReadLine();
+                if (amal == "+")
+                {
+                    Console.WriteLine("Natija: " + (a + b));
+                }
+                else if (amal == "-")
+                {
+                    Console.WriteLine("Natija: " + (a - b));
+                }
+                else if (amal == "*")
+                {
+                    Console.WriteLine("Natija: " + (a * b));
+                }
+                else if (amal == "/")
+                {
+                    if (b == 0)
+                        Console.WriteLine("0 ga bolish mumkin emas ");
+                    else
+                        Console.WriteLine("Natija: " + (a / b));
+                }
                 else
-                    Console.WriteLine("Natija: " + (a / b));
+                {
+                    Console.WriteLine("Notogri amal kiritildi ");
+                }
+                break;
             }
-            else
+        case 2:
             {
-                Console.WriteLine("Notogri amal kiritildi ");
-            }
-            break;
-        }
-    case 2:
-        {
-            Console.WriteLine("Uchburchakni yuzini topish Geron Formulasi");
-            Console.Write(" a tomon : ");
-            double a = Convert.ToDouble(Console.ReadLine());
-            Console.Write(" b tomon : ");
-            double b = Convert.ToDouble(Console.ReadLine());
-            Console.Write(" c tomon : ");
-            double c = Convert.ToDouble(Console.ReadLine());
-            if (a + b > c && a + c > b && b + c > a)
-            {
-                double p = (a + b + c) / 2;
-                double S = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+                Console.WriteLine("Uchburchakni yuzini topish Geron Formulasi");
+                Console.Write(" a tomon : ");
+                double a = Convert.ToDouble(Console.ReadLine());
+                Console.Write(" b tomon : ");
+                double b = Convert.ToDouble(Console.ReadLine());
+                Console.Write(" c tomon : ");
+                double c = Convert.ToDouble(Console.ReadLine());
+                if (a + b > c && a + c > b && b + c > a)
+                {
+                    double p = (a + b + c) / 2;
+                    double S = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
 
-                Console.WriteLine("Yuza: " + S);
+                    Console.WriteLine("Yuza: " + S);
+                }
+                else
+                {
+                    Console.WriteLine("Bunday uchburchak mavjud emas ");
+                }
+                break;
             }
-            else
+        case 3:
             {
-                Console.WriteLine("Bunday uchburchak mavjud emas ");
+                Console.Write("Sonning kvadratini topuvchi dastur : ");
+                string inputFirst = Console.ReadLine();
+                int a = Convert.ToInt32(inputFirst);
+                Console.Write("b sonini kiriting : ");
+                string inputSecond = Console.ReadLine();
+                int b = Convert.ToInt32(inputSecond);
+                Console.WriteLine(Math.Pow(a,b));
+                break;   
             }
+        default:
+            Console.WriteLine("Bunday tugma mavjud emas ");
             break;
-        }
-    case 3:
-        {
-            Console.Write("Sonning kvadratini topuvchi dastur : ");
-            string inputFirst = Console.ReadLine();
-            int a = Convert.ToInt32(inputFirst);
-            Console.Write("b sonini kiriting : ");
-            string inputSecond = Console.ReadLine();
-            int b = Convert.ToInt32(inputSecond);
-            Console.WriteLine(Math.Pow(a,b));
-            break;   
-        }
-     default:
-        Console.WriteLine("Bunday tugma mavjud emas ");
-        break;
 
+    }
+
+    Console.Write("\n Yana davom etassizmi ? (Yes/No): ");
+    string answer = Console.ReadLine().ToLower();
+    if(answer == "No" || answer == "n")
+    {
+        s = false;
+    }
+    Console.WriteLine();
 }
 
--- dars6.cs
+++
@@ -1,8 +0,0 @@
-
-// bir xonlai ikkita son kiritilganda ixtiyoriy natijasni ekranga chiqarish 
-string misol = Console.ReadLine();
-string son1 = misol[0];
-string son2 = misol[2];
-string operator1 = misol[1];
-
-
