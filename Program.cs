74e5b14f6b685ee677c83cb6021901b5f1d28c62
Ergashev21 <t.ergashev2120@gmail.com>
2026-05-08 12:47:07 +0500

    Tayyor 6 gacha

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ class Program
         while(s)
         {
             Console.Clear();
-            Console.WriteLine("Ushbu dastur orqali \n \t 1: AddNumber \n\t 2: DivideNumber \n\t 3:CalculateGeron \n\t 4 : Tub son  ");
+            Console.WriteLine("Ushbu dastur orqali \n \t 1: AddNumber \n\t 2: DivideNumber \n\t 3:CalculateGeron \n\t 4 : Eng kat sonta  \n\t 5:Faktarial \n\t 6: Yigindi  ");
             string str = Console.ReadLine();
             int userInput = Convert.ToInt32(str);
             switch (userInput)
@@ -50,8 +50,32 @@ class Program
                     }
                 case 4:
                     {
-                        
-                     break;
+                        Console.Write("A sonini kiriting : ");
+                        string str1 = Console.ReadLine();
+                        int  a = Convert.ToInt32(str1);
+                        Console.Write("B sonini kiriting : ");
+                        string str2 = Console.ReadLine();
+                        int  b = Convert.ToInt32(str2);
+                        string str3 = Console.ReadLine();
+                        int  c = Convert.ToInt32(str3);
+                        Console.WriteLine(EngKattaSon(a,b,c));
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.Write("A sonini kiriting : ");
+                        string str1 = Console.ReadLine();
+                        int a = Convert.ToInt32(str1);
+                        Console.WriteLine(Factarial(a));
+                        break;
+                    }
+                case 6:
+                    {
+                        Console.Write("A sonini kiriting : ");
+                        string str1 = Console.ReadLine();
+                        int a = Convert.ToInt32(str1);
+                        Console.WriteLine(Yigindi(a));
+                        break;
                     }
             }
 
@@ -86,9 +110,30 @@ class Program
                     return 0;
                 }
         }
+    static int  EngKattaSon (int a,int b,int c)
+        {
+            return Math.Max(a,Math.Max(b,c));
+        }
             
     }
-
+    static long Factarial(int a)
+    {
+        long res = 1;
+        for (int i = 1;i <= a; i++)
+        {
+            res *= i;
+        }
+        return res;
+    }
+    static long Yigindi(int a)
+    {
+        long res = 0;
+        for (int i = 0;i <= a; i++)
+        {
+            res += i;
+        }
+        return res;
+    }
     
 }
 
--- obj/Debug/net10.0/GeometryGuru.AssemblyInfo.cs
+++ obj/Debug/net10.0/GeometryGuru.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeometryGuru")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c22b939c395e73a27bb6f0b996eea2f87667885")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd6571bc8359c6d2ce384bab1b1508524b317b2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeometryGuru")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeometryGuru")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
