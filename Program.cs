ea3a4edc23369c4e16cc19858e3b42331cbf1fa2
Ergashev21 <t.ergashev2120@gmail.com>
2026-05-13 02:46:03 +0500

    Hammasi Tayyor

--- Program.cs
+++ Program.cs
@@ -4,8 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        bool s = true;
-        while(s)
+        
+        do
         {
             Console.Clear();
             Console.WriteLine("Ushbu dastur orqali \n \t 1: AddNumber \n\t 2: DivideNumber \n\t 3:CalculateGeron \n\t 4 : Eng kat sonta  \n\t 5:Faktarial \n\t 6: Yigindi  ");
@@ -15,124 +15,138 @@ class Program
             {
                 case 1:
                     {
-                        Console.Write("A sonini kiriting : ");
-                        string str1 = Console.ReadLine();
-                        double a = Convert.ToDouble(str1);
-                        Console.Write("B sonini kiriting : ");
-                        string str2 = Console.ReadLine();
-                        double b = Convert.ToDouble(str2);
-                        Console.WriteLine(AddNumber(a,b));
+                       
+                        AddNumber();
                         break;
                     }
                 case 2:
                     {
-                        Console.Write("A sonini kiriting : ");
-                        string str1 = Console.ReadLine();
-                        double a = Convert.ToDouble(str1);
-                        Console.Write("B sonini kiriting : ");
-                        string str2 = Console.ReadLine();
-                        double b = Convert.ToDouble(str2);
-                        Console.WriteLine(DivideNumber(a,b));
+                        
+                        DivideNumber();
                         break;
                     }
                 case 3:
                     {   
-                        Console.Write("A sonini kiriting : ");
-                        string str1 = Console.ReadLine();
-                        double a = Convert.ToDouble(str1);
-                        Console.Write("B sonini kiriting : ");
-                        string str2 = Console.ReadLine();
-                        double b = Convert.ToDouble(str2);
-                        string str3 = Console.ReadLine();
-                        double c = Convert.ToDouble(str3);
-                        Console.WriteLine(CalculateGeron(a,b,c));
+                        
+                        CalculateGeron();
                         break;   
                     }
                 case 4:
                     {
-                        Console.Write("A sonini kiriting : ");
-                        string str1 = Console.ReadLine();
-                        int  a = Convert.ToInt32(str1);
-                        Console.Write("B sonini kiriting : ");
-                        string str2 = Console.ReadLine();
-                        int  b = Convert.ToInt32(str2);
-                        string str3 = Console.ReadLine();
-                        int  c = Convert.ToInt32(str3);
-                        Console.WriteLine(EngKattaSon(a,b,c));
+                        
+                        EngKattaSon();
                         break;
                     }
                 case 5:
                     {
-                        Console.Write("A sonini kiriting : ");
-                        string str1 = Console.ReadLine();
-                        int a = Convert.ToInt32(str1);
-                        Console.WriteLine(Factarial(a));
+                        
+                        Factarial();
                         break;
                     }
                 case 6:
                     {
-                        Console.Write("A sonini kiriting : ");
-                        string str1 = Console.ReadLine();
-                        int a = Convert.ToInt32(str1);
-                        Console.WriteLine(Yigindi(a));
+                       
+                        Yigindi();
                         break;
                     }
             }
 
             Console.Write("\n Yana davom etassizmi ? (Yes/No): ");
             string answer = Console.ReadLine().ToLower();
-            if(answer == "No" || answer == "n")
+            if(answer == "No" || answer == "n" || answer == "no")
             {
                 s = false;
             }
             Console.WriteLine();
-        }
-    static double AddNumber( double x,double y)
+        } while(s);
+    static void AddNumber( )
         { 
-            return Math.Abs(x) + Math.Abs(y);
+            Console.Write("A sonini kiriting : ");
+            string str1 = Console.ReadLine();
+            double a = Convert.ToDouble(str1);
+            Console.Write("B sonini kiriting : ");
+            string str2 = Console.ReadLine();
+            double b = Convert.ToDouble(str2);
+            Console.WriteLine($"Natija : {a+b}");
         }
-     static double DivideNumber( double x,double y)
+     static void DivideNumber( )
         {
-            return x / y;
+            Console.Write("A sonini kiriting : ");
+            string str1 = Console.ReadLine();
+            double a = Convert.ToDouble(str1);
+            Console.Write("B sonini kiriting : ");
+            string str2 = Console.ReadLine();
+            double b = Convert.ToDouble(str2);
+            Console.WriteLine($"Natija : {a/b}");
+            
         }
-    static double CalculateGeron(double a,double b, double c)
+    static void CalculateGeron()
         {
+            Console.Write("A sonini kiriting : ");
+            string str1 = Console.ReadLine();
+            double a = Convert.ToDouble(str1);
+            Console.Write("B sonini kiriting : ");
+            string str2 = Console.ReadLine();
+            double b = Convert.ToDouble(str2);
+            string str3 = Console.ReadLine();
+            double c = Convert.ToDouble(str3);
             
              if (a + b > c && a + c > b && b + c > a)
                 {
                     double p = (a + b + c) / 2;
                     double result = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
-                    return result;
+                    Console.WriteLine($"Natija : {result}");
                             
                 }
             else
                 {
-                    return 0;
+                    Console.WriteLine($"Natija : {0}");
                 }
         }
-    static int  EngKattaSon (int a,int b,int c)
+    static void  EngKattaSon ()
         {
-            return Math.Max(a,Math.Max(b,c));
+            Console.Write("A sonini kiriting : ");
+            string str1 = Console.ReadLine();
+            int  a = Convert.ToInt32(str1);
+            Console.Write("B sonini kiriting : ");
+            string str2 = Console.ReadLine();
+            int  b = Convert.ToInt32(str2);
+            Console.Write("C sonini kiriting : ");
+            string str3 = Console.ReadLine();
+            int  c = Convert.ToInt32(str3);
+            Console.WriteLine( Math.Max(a,Math.Max(b,c)));
         }
             
     }
-    static long Factarial(int a)
+
+    private static void AddNumber()
     {
+        throw new NotImplementedException();
+    }
+
+    static void Factarial()
+    {
+        Console.Write("A sonini kiriting : ");
+        string str1 = Console.ReadLine();
+        int a = Convert.ToInt32(str1);
         long res = 1;
         for (int i = 1;i <= a; i++)
         {
             res *= i;
         }
-        return res;
+        Console.WriteLine($"Natija {res}" );
     }
-    static long Yigindi(int a)
+    static void Yigindi()
     {
+        Console.Write("A sonini kiriting : ");
+        string str1 = Console.ReadLine();
+        int a = Convert.ToInt32(str1);
         long res = 0;
         for (int i = 0;i <= a; i++)
         {
             res += i;
         }
-        return res;
+        Console.WriteLine($"Natija {res}" );
     }
     
 }
